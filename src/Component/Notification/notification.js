32808d2bc19daeeba8cf83b4f8da3d1e7dcd8549
Martin <hello@northrook.com>
2024-08-25 07:08:16 +0100

    mend

--- src/Component/Notification/notification.js
+++ src/Component/Notification/notification.js
@@ -1,63 +1,61 @@
 function notification() {
 
-    const timeInMs = 250;
+	const timeInMs = 250;
 
-    /** @var {NodeListOf<ToastElement>} notifications */
-    const notifications = document.querySelectorAll( 'toast' );
+	const notifications = $all( 'toast' );
 
+	if ( !notifications.length ) return;
 
-    if ( !notifications.length ) return;
+	const container = $get( 'section#notifications', {class: 'fixed top right', role: 'list'} ).prependTo();
 
-    /** @var {ToastContainer} container */
-    const container = $get( 'section#notifications', {class: 'fixed top right', role: 'list'} );
+	// console.log( container );
 
-    // console.log( container );
+	notifications.forEach( ( toast, i ) => {
 
-    notifications.forEach( ( toast, i ) => {
+		if ( !toast.style.getPropertyValue( '--timeout' ) ) {
 
-        if ( !toast.style.getPropertyValue( '--timeout' ) ) {
+			toast.classList.add( 'hidden' );
 
-            toast.classList.add( 'hidden' );
+			const remove = () => {
+				const parent = toast.parentElement;
+				const transition = elementAnimations( toast );
 
-            const remove = () => {
-                const parent = toast.parentElement;
-                const transition = elementAnimations( toast );
+				toast.classList.add( 'hidden' );
 
-                toast.classList.add( 'hidden' );
+				toast.addEventListener( 'transitionend', ( transitionend ) => {
+					if ( transitionend.propertyName !== transition.longest.property ) return;
+					toast.remove();
+					if ( parent && parent.children.length === 0 ) parent.remove();
+				} );
+			}
 
-                toast.addEventListener( 'transitionend', ( transitionend ) => {
-                    if ( transitionend.propertyName !== transition.longest.property ) return;
-                    toast.remove();
-                    if ( parent && parent.children.length === 0 ) parent.remove();
-                } );
-            }
+			const timeout = parseInt( toast.getAttribute( 'timeout' ) ?? '0' );
 
-            const timeout = parseInt( toast.getAttribute( 'timeout' ) ?? '0' );
+			setTimeout( () => {
+				toast.classList.remove( 'hidden' );
 
-            setTimeout( () => {
-                toast.classList.remove( 'hidden' );
+				if ( timeout ) {
 
-                if ( timeout ) {
+					const timeoutBar = $new( '<div class="progress-bar"></div>' );
+					toast.style.setProperty( '--timeout', timeout + 'ms' );
+					timeoutBar.appendTo( toast )
 
-                    const timeoutBar = $make( '<div class="progress-bar"></div>' );
-                    toast.style.setProperty( '--timeout', timeout + 'ms' );
-                    toast.appendChild( timeoutBar.element );
+					toast.addEventListener( 'click', () => {
+						timeoutBar.fadeOut();
+						toast.setAttribute( 'timeout', 'stopped' );
+						toast.removeEventListener( 'animationend', remove );
+					}, {once: true} );
+					toast.addEventListener( 'animationend', remove, {once: true} );
+				}
+			}, timeInMs + ( i * ( timeInMs / 2 ) ) );
 
-                    toast.addEventListener( 'click', () => {
-                        timeoutBar.fadeOut();
-                        toast.setAttribute( 'timeout', 'stopped' );
-                        toast.removeEventListener( 'animationend', remove );
-                    }, {once: true} );
-                    toast.addEventListener( 'animationend', remove, {once: true} );
-                }
-            }, timeInMs + ( i * ( timeInMs / 2 ) ) );
+			$( 'button.close', toast ).on( 'click', remove );
+			// toast.querySelector( 'button.close' )?.addEventListener( 'click', remove );
 
-            toast.querySelector( 'button.close' )?.addEventListener( 'click', remove );
+			container.appendChild( toast );
 
-            container.appendChild( toast );
-
-        }
-    } );
+		}
+	} );
 }
 
 App.on.ContentLoaded( notification );
